bf7c61dfd43e8549babc29ce60041967b427304e
StepanIP <scrupnichuk@gmail.com>
2023-12-24 16:10:16 +0200

    initial commit

---
+++ src/main/java/com/example/expandapitesttask/ExpandApiTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.expandapitesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpandApiTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpandApiTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/expandapitesttask/ExpandApiTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expandapitesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpandApiTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
