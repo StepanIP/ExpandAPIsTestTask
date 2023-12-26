7559b55952cd1136fbcdfefaf38c11bacbaad792
StepanIP <scrupnichuk@gmail.com>
2023-12-26 11:39:53 +0200

    created user controller, changet server port and added token variable to context

---
+++ src/main/java/com/example/expandapitesttask/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.example.expandapitesttask.controllers;
+
+import com.example.expandapitesttask.dto.request.UserRequest;
+import com.example.expandapitesttask.dto.response.JwtAuthenticationResponse;
+import com.example.expandapitesttask.service.security.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user") //usually it is /api/v1/ but I did as in the instruction
+public class UserController {
+
+    private final AuthenticationService authenticationService;
+
+    public UserController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<JwtAuthenticationResponse> createUser(@RequestBody UserRequest request) {
+        return ResponseEntity.ok(authenticationService.signup(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<JwtAuthenticationResponse> authenticateUser(@RequestBody UserRequest request) {
+        return ResponseEntity.ok(authenticationService.signin(request));
+    }
+
+
+}
